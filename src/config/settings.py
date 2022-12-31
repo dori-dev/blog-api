d8d77ee9b18b6e84894e8808bc6e90201e6f3637
dori-dev <mr.dori.dev@gmail.com>
2022-12-31 17:31:10 +0330

    create api directory

--- src/config/settings.py
+++ src/config/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
+}
