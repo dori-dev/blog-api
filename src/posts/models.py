a03bdc166bb030b0718b572ea878db161958cdda
dori-dev <mr.dori.dev@gmail.com>
2022-12-31 15:40:54 +0330

    add post model

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(
+        max_length=64,
+    )
+    body = models.TextField()
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    def __str__(self) -> str:
+        return self.title
