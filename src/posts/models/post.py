56da4e1df565d3c6f7538c3bdc5b5f62d83ede41
dori-dev <mr.dori.dev@gmail.com>
2022-12-31 16:21:03 +0330

    add models folder

---
+++ src/posts/models/post.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.translation import gettext as _
+from django.contrib.auth import get_user_model
+
+from utils import GeneralModel
+
+User = get_user_model()
+
+
+class Post(GeneralModel):
+    author = models.ForeignKey(
+        User,
+        verbose_name=_('Author'),
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+    title = models.CharField(
+        max_length=64,
+    )
+    body = models.TextField(
+        verbose_name=_('Body'),
+    )
+
+    def __str__(self) -> str:
+        return self.title
