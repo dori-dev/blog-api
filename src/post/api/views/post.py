fecf6578481c54652cc7e57e64b33fda9b0b9ea5
dori-dev <mr.dori.dev@gmail.com>
2023-01-01 16:41:02 +0330

    set permission for views

--- src/post/api/views/post.py
+++ src/post/api/views/post.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from post.models import Post
 from post.api.serializers import PostSerializer
@@ -7,8 +7,14 @@ from post.api.serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+    )
