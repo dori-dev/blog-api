f6e47411890afd8ea853710ace3e164c7de636e5
dori-dev <mr.dori.dev@gmail.com>
2023-01-02 22:49:19 +0330

    add core api for api doc

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,6 +3,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+
+
+TITLE = 'Blog API'
+DESCRIPTION = 'Usage guide for blog api'
 
 v1_urlpatterns = [
     path('post/', include('post.api.urls', namespace='post')),
@@ -19,6 +24,16 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path(
         'schema/',
-        get_schema_view(title='Blog API'),
-    )
+        get_schema_view(
+            TITLE,
+            DESCRIPTION,
+        ),
+    ),
+    path(
+        'docs/',
+        include_docs_urls(
+            TITLE,
+            DESCRIPTION,
+        ),
+    ),
 ]
