79e13dd71875bf45d0888b2582135a86da7fbbdd
dori-dev <mr.dori.dev@gmail.com>
2023-01-02 22:42:38 +0330

    add schema

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,6 +2,7 @@
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 v1_urlpatterns = [
     path('post/', include('post.api.urls', namespace='post')),
@@ -16,4 +17,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(v1_urlpatterns)),
     path('api-auth/', include('rest_framework.urls')),
+    path(
+        'schema/',
+        get_schema_view(title='Blog API'),
+    )
 ]
